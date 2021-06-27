ad577698e7efe5c5dc8057b03339f75e762a59c3
Nino Cehtelj Kristl <cehteljkristl.nino@gmail.com>
2021-06-27 14:23:06 +0200

    Add university project

---
+++ Seminarska_Naloga/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zival;
+using System.IO;
+
+namespace Seminarska_Naloga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Zivali> ziv = new List<Zivali>();
+
+            do
+            {
+                Zivali.Meni();
+                int x = int.Parse(Console.ReadLine());
+
+                switch (x)
+                {
+                    case 1: //Dodajanje
+                        Zivali z = new Zivali();
+                        Console.WriteLine();
+                        Console.Write("Ime zivali: ");
+                        z.ImeZivali = Console.ReadLine();
+                        Console.Write("Starost zivali: ");
+                        z.StarostZivali = int.Parse(Console.ReadLine());
+                        Console.Write("Vrsta zivali: ");
+                        z.VrstaZivali = Console.ReadLine();
+                        ziv.Add(z);
+                        break;
+                    case 2: //Izbris izbrane zivali
+                        Console.Write("Izbrises lahko zivali med 0 in {0}.Katero zelis izbrisat? ", ziv.Count-1);
+                        int izbris = int.Parse(Console.ReadLine());
+                        if (izbris > ziv.Count || izbris < 0)
+                        {
+                            Console.WriteLine("Zival ne tem mestu ne obstaja!!!");
+                            break;
+                        }
+                        else
+                        {
+                            ziv.RemoveAt(izbris);
+                            Console.WriteLine("Izbrana zival je bila izbrisana!");
+                            break;
+                        }
+                    case 3: //Spremeni izbran objekt
+                        Console.Write("Spremenis lahko zivali med 0 in {0}.Katero zelis spremenit? ", ziv.Count-1);
+                        int spremeni = int.Parse(Console.ReadLine());
+                        if (spremeni > ziv.Count || spremeni < 0)
+                        {
+                            Console.WriteLine("Zival ne tem mestu ne obstaja!");
+                            break;
+                        }
+                        else
+                        {
+                            ziv.RemoveAt(spremeni);
+                            Zivali z1 = new Zivali();
+                            Console.Write("Ime zivali: ");
+                            z1.ImeZivali = Console.ReadLine();
+                            Console.Write("Starost zivali: ");
+                            z1.StarostZivali = int.Parse(Console.ReadLine());
+                            Console.Write("Vrsta zivali: ");
+                            z1.VrstaZivali = Console.ReadLine();
+                            ziv.Insert(spremeni, z1);
+                            Console.WriteLine("Zival na mestu {0} je bila spremenjena!", spremeni);
+                            break;
+                        }
+                    case 4: //Nalozi list v datoteko !!
+                        string path = @"C:\\Users\\Nino PR\\Desktop\\PRO2\\txt\\text.txt";
+                        int zi = 1;
+                        using (TextWriter tw = new StreamWriter(path))
+                        {
+                            foreach (var item in ziv)
+                            {
+                                tw.WriteLine("{0}. Zival:",zi);
+                                tw.WriteLine(item.ToString());
+                                tw.WriteLine();
+                                zi++;
+                            }
+                            tw.Close();
+                        }
+                        Console.WriteLine("Zivali so bile nalozene v datoteko!");
+                        break;
+                    case 5: //Nalozi datoteko v list !! 
+                        Console.WriteLine();
+                        Console.WriteLine("Za vstavljanje zivali v datoteko moramo uporabiti >vejico< kot locilo v naslednjiem vrstnem redu.");
+                        Console.WriteLine("Primer: ImeZivali,StarostZivali,VrstaZivali");
+                        Console.WriteLine("Ce razumete pritisnite ENTER za nadaljevanje!");
+                        string pathDat = @"C:\\Users\\Nino PR\\Desktop\\PRO2\\txt\\zivaliVen.txt";
+                        int m = 0;
+                        using (TextReader sr = new StreamReader(pathDat))
+                        {
+                            string linja = sr.ReadLine();
+                            if(linja == null)
+                            {
+                                Console.WriteLine("Datoteka je prazna! Nalozeno ni bilo nic!");
+                                break;
+                            }
+                            else { }
+
+                            while (linja != null)
+                            {
+                                Zivali zDat = new Zivali();
+                                string[] lines = linja.Split(',');
+                                zDat.ImeZivali = lines[0];
+                                zDat.StarostZivali = Int32.Parse(lines[1]);
+                                zDat.VrstaZivali = lines[2];
+                                ziv.Add(zDat);
+                                linja = sr.ReadLine();
+                                m++;
+                            };
+                        }
+                        Console.WriteLine("Zivali so bile nalozene iz datoteke!");
+                        break;
+                    case 6: //Izpis vseh zivali
+                        int i = 1;
+                        if (ziv.Count <= 0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("List zivali je prazen, nemorem nic izpisati!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Izpis Vseh zivali: ");
+                            foreach (var zival in ziv)
+                            {
+                                Console.WriteLine("{0}. Zival:", i);
+                                zival.IzpisiZival();
+                                Console.WriteLine();
+                                i++;
+                            }
+                        }
+                        break;
+                    case 7: //Izpis izbrane zivali
+                        Console.Write("Izpises lahko zivali med 0 in {0}.Katero zelis izpisat? ", ziv.Count-1);
+                        int izpisi = int.Parse(Console.ReadLine());
+                        if (izpisi >= ziv.Count || izpisi < 0)
+                        {
+                            Console.WriteLine("Zivali ne tem mestu ne obstaja!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zival na {0}, mestu: ", izpisi);
+                            ziv[izpisi].IzpisiZival();
+                            break;
+                        }
+
+                    case 8: //Izbrisi vse objekte
+                        ziv.Clear();
+                        Console.WriteLine();
+                        Console.WriteLine("Vse zivali v listu so bile izbrisane. Zdaj je list prazen");
+                        break;
+                    case 9: //Izpis stevila objektov
+                        Console.WriteLine();
+                        Console.WriteLine("V list je trenutno {0} shranjenih zivali.", ziv.Count());
+                        break;
+                    case 10: //Sortiranje objektov po dolocenem kriteriju
+                        Zivali.Sortiranja();
+                        int sort = int.Parse(Console.ReadLine());
+                        switch (sort)
+                        {
+                            case 1: //Starost naracajoce
+                                ziv.Sort((j, y) => j.StarostZivali.CompareTo(y.StarostZivali));
+                                Console.WriteLine("List zivali je zdaj sortirano po starosti narascajoce.");
+                                break;
+                            case 2: //Startost padajoce
+                                ziv = ziv.OrderByDescending(t => t.StarostZivali).ToList();
+                                Console.WriteLine("List zivali je zdaj sortirano po starosti padajoce.");
+                                break;
+                            case 3: //Ime A->Z
+                                ziv.Sort((j, y) => j.ImeZivali.CompareTo(y.ImeZivali));
+                                Console.WriteLine("List zivali je zdaj sortirano po imenih A->Z.");
+                                break;
+                            case 4://Ime Z->A
+                                ziv.Sort((j, y) =>y.ImeZivali.CompareTo(j.ImeZivali));
+                                Console.WriteLine("List zivali je zdaj sortirano po imenih Z->A.");
+                                break;
+                                //Sortiranje po vecih atributih
+                            case 5: //To sortiranje pride v upostev ce je vec zivali z istim imenom ali isto starostjo
+                                ziv = ziv.OrderBy(y => y.StarostZivali).ThenBy(y => y.ImeZivali).ToList();
+                                Console.WriteLine("Zivali bodo prvo sortirane po strosti padajoce potem po imenih A-Z.");
+                                break;
+                            case 6: //To sortiranje pride v upostev ce je vec zivali z istim imenom ali isto starostjo
+                                ziv = ziv.OrderBy(y => y.ImeZivali).ThenBy(y => y.StarostZivali).ToList();
+                                Console.WriteLine("Zivali bodo prvo sortirane po imenu A-Z potem po letih padajoce.");
+                                break;
+                            default:
+                                Console.WriteLine("Sortiranje z to predstevilko ne obstaja!!!");
+                                break;
+                        }
+                        break;
+                    case 11: //Zakljuci program
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Moznost z to stevilko ne obsataja!");
+                        break;
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("Po pregledu rezultata pritisnite katerokoli tipko za nadaljevanje!");
+                Console.ReadKey(true);
+            } while (true);
+
+        }
+    }
+}
---
+++ Seminarska_Naloga/Zivali.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seminarska_Naloga;
+using System.IO;
+
+namespace Zival
+{
+    class Zivali
+    {
+        private int starostZivali;
+        private string vrstaZivali;
+        private string imeZivali;
+
+        public int StarostZivali
+        {
+            get { return starostZivali; }
+            set { starostZivali = value; }
+        }
+        public string VrstaZivali
+        {
+            get { return vrstaZivali; }
+            set { vrstaZivali = value; }
+        }
+        public string ImeZivali
+        {
+            get { return imeZivali; }
+            set { imeZivali = value; }
+        }
+
+        public Zivali() //Privzeti
+        {
+            StarostZivali = 0;
+            VrstaZivali = null;
+            ImeZivali = null;
+        }
+        public Zivali(int starostZivali, string vrstaZivali, string imeZivali) //Pretvorbeni
+        {
+            StarostZivali = starostZivali;
+            VrstaZivali = vrstaZivali;
+            ImeZivali = imeZivali;
+        }
+        public Zivali(Zivali ziv) //Kopirni
+        {
+            StarostZivali = ziv.StarostZivali;
+            VrstaZivali = ziv.VrstaZivali;
+            ImeZivali = ziv.ImeZivali;
+        }
+        ~Zivali() //Destruktor
+        {
+            if (StarostZivali != 0) starostZivali = StarostZivali;
+            else if (VrstaZivali != null) vrstaZivali = VrstaZivali;
+            else if (ImeZivali != null) imeZivali = ImeZivali;
+
+            Console.WriteLine("Zival- Ime: {0}, Vrsta: {1}, Starost: {2}. Je bila izbrisana!",imeZivali,vrstaZivali,starostZivali);
+        }
+
+    
+        //ToString() override
+        public override string ToString()
+        {
+            return "Ime zivali: " + ImeZivali + "\nStarost zivali: " + StarostZivali.ToString() + 
+                "\nVrsta zivali: " + VrstaZivali + ".";
+        }
+        
+        //Metode       
+        public void IzpisiZival()
+        {
+                Console.WriteLine("Ime: {0}", imeZivali);
+                Console.WriteLine("Starost: {0}", starostZivali);
+                Console.WriteLine("Vrsta zivali: {0}", vrstaZivali);      
+        }
+
+        public static void Meni() //Meni
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Dodaj novo zival");
+            Console.WriteLine("2. Izbrisi izbrano zival");
+            Console.WriteLine("3. Spremeni izbran objekt");
+            Console.WriteLine("4. Shrani zivali v datoteko");
+            Console.WriteLine("5. Nalozi datoteko v list zivali");
+            Console.WriteLine("6. Izpis vseh zivali");
+            Console.WriteLine("7. Izpis izbrano zival");
+            Console.WriteLine("8. Izbrisi vse zivali iz lista");
+            Console.WriteLine("9. Izpis stevilo vpisanih objektov");
+            Console.WriteLine("10. Uporaba podmenija za sortiranje");
+            Console.WriteLine("11. Koncaj program");
+            Console.WriteLine();
+            Console.Write("Vpisi stevilko pred izbrano moznostjo: ");
+        }
+        public static void Sortiranja() //Podmeni
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Po letih narascajoce");
+            Console.WriteLine("2. Po letih padajoce");
+            Console.WriteLine("3. Po imenu (A->Z)");
+            Console.WriteLine("4. Po imenu (Z->A)");
+            Console.WriteLine("5. Prvo po starosti padajoce nato pa se po imenu A-Z");
+            Console.WriteLine("6. Prvo po imenu A-Z nato pa se po starosti padajoce");
+            Console.Write("Katero sortiranje zelis izbrati?");
+        }
+
+        
+    }
+}
